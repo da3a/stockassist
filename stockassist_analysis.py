f87b4f7ccb66972dc9220414982d039fa8dbff9c
David Wallwin <david.wallwin@gmail.com>
2017-11-26 23:03:50 +0000

    update

--- stockassist_analysis.py
+++ stockassist_analysis.py
@@ -26,10 +26,11 @@ def get_top_coefficients(tickers,top, start_time, end_time):
             continue
         model_data = pd.merge(market_data, df, how='inner', on=['Date'])
         model_data['Date'] = pd.to_datetime(model_data['Date'], format='%Y-%m-%d')
-        model_data.set_index('Date', inplace=True)
-        #model_data = model_data.ix[pd.to_datetime('2017-11-01',format='%Y-%m-%d'):pd.to_datetime('2017-11-22', format='%Y-%m-%d')]
-        model_data = model_data.ix[start_time:end_time]
-        #model_data.ix[start_time, end_time]
+        model_data.set_index('Date', inplace=True)       
+        model_data = model_data.ix[pd.to_datetime('{:%Y-%m-%d}'.format(start_time)):pd.to_datetime('{:%Y-%m-%d}'.format(end_time))]
+        model_data = model_data[model_data['Returns_x'] > 0]
+        if model_data.empty or len(model_data) < 2:
+            continue       
         model_xData = model_data['Returns_x'][:-1].values.reshape(-1,1)
         model_yData = model_data['Returns_y'][:-1]
         model = linear_model.LinearRegression()
@@ -51,8 +52,9 @@ def plot_figures(tickers):
         model_data = pd.merge(market_data, df, how='inner', on=['Date'])
         model_data['Date'] = pd.to_datetime(model_data['Date'], format='%Y-%m-%d')
         model_data.set_index('Date', inplace=True)
-        #model_data = model_data.ix[pd.to_datetime('2017-11-01',format='%Y-%m-%d'):pd.to_datetime('2017-11-22', format='%Y-%m-%d')]
-        model_data = model_data.ix[start_time:end_time]
+        model_data = model_data.ix[pd.to_datetime('{:%Y-%m-%d}'.format(start_time)):pd.to_datetime('{:%Y-%m-%d}'.format(end_time))]
+        if model_data.empty or len(model_data) < 2:
+            continue       
         model_xData = model_data['Returns_x'][:-1].values.reshape(-1,1)
         model_yData = model_data['Returns_y'][:-1]
         model = linear_model.LinearRegression()
@@ -65,10 +67,10 @@ def plot_figures(tickers):
         ctr = ctr+1
     plt.show()
 
-#start_time = dt.datetime(2017,1,1)
-#end_time = dt.datetime(2017,11,1)
-start_time = '2017-11-01'
-end_time = '2017-11-22'
+start_time = dt.datetime(2017,11,1)
+end_time = dt.datetime(2017,11,22)
+#start_time = '2017-11-01'
+#end_time = '2017-11-22'
 tickers = sa_scrape.get_all_ticker_symbols_from_file()
-top_tickers = get_top_coefficients(tickers[:50],5, start_time, end_time)
+top_tickers = get_top_coefficients(tickers[:500],25, start_time, end_time)
 plot_figures(dict(top_tickers).keys())
