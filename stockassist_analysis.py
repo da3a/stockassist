d5f68534177d61659f5401cf03a2d3d5e878deb8
David Wallwin <david.wallwin@gmail.com>
2017-11-28 23:49:02 +0000

    update

--- stockassist_analysis.py
+++ stockassist_analysis.py
@@ -9,6 +9,7 @@ import pandas as pd
 import numpy as mp
 import datetime as dt
 import matplotlib.pyplot as plt
+import pickle
 
 from sklearn import datasets, linear_model
 from operator import itemgetter
@@ -16,9 +17,18 @@ from heapq import nlargest
 """
 """
 
+MARKET = 'nasdaq'
+ROOT = 'c:/dawa/stockassist'
+TICKER_FILE = '{}/{}_TOP_12.pickle'.format(ROOT, MARKET)
+THRESHOLD_SCORE = 0.4
 
-def get_top_coefficients(tickers,top, start_time, end_time):
+def get_top_coefficients(tickers,top, start_time, end_time, loadfromfile =False):   
     ranking = {}
+    if loadfromfile:
+        with open(TICKER_FILE,'rb') as f:
+            ranking = pickle.load(f)
+        return ranking
+
     market_data = sa_scrape.load_market_data()
     for ticker in tickers:
         df = sa_scrape.get_ticker_historical_data(ticker, start_time, end_time)
@@ -28,7 +38,7 @@ def get_top_coefficients(tickers,top, start_time, end_time):
         model_data['Date'] = pd.to_datetime(model_data['Date'], format='%Y-%m-%d')
         model_data.set_index('Date', inplace=True)       
         model_data = model_data.ix[pd.to_datetime('{:%Y-%m-%d}'.format(start_time)):pd.to_datetime('{:%Y-%m-%d}'.format(end_time))]
-        model_data = model_data[model_data['Returns_x'] > 0]
+        # model_data = model_data[model_data['Returns_x'] > 0]
         if model_data.empty or len(model_data) < 2:
             continue       
         model_xData = model_data['Returns_x'][:-1].values.reshape(-1,1)
@@ -36,8 +46,14 @@ def get_top_coefficients(tickers,top, start_time, end_time):
         model = linear_model.LinearRegression()
         model.fit(model_xData, model_yData)
         print('coefficient for {} is {}'.format(ticker, model.coef_))
-        ranking[ticker] = model.coef_   
-    return sorted(ranking.items(), key=itemgetter(1),reverse=True)[:top]
+        score = model.score(model_xData, model_yData)
+        if score < THRESHOLD_SCORE:
+            continue
+        ranking[ticker] = model.coef_
+    sorted_dict = sorted(ranking.items(), key=itemgetter(1),reverse=True)[:top] 
+    with open(TICKER_FILE, 'wb') as f:
+        pickle.dump(sorted_dict,f)
+    return sorted_dict
 
 def plot_figures(tickers):
     market_data = sa_scrape.load_market_data()
@@ -59,18 +75,21 @@ def plot_figures(tickers):
         model_yData = model_data['Returns_y'][:-1]
         model = linear_model.LinearRegression()
         model.fit(model_xData, model_yData)
+        score = model.score(model_xData, model_yData)
         ax = plt.subplot(5,5,ctr)
-        ax.set_title(ticker + ' ' + str(model.coef_))   
+        ax.set_title(ticker + ' ' + str(model.coef_) + ' ' + '{:0.2f}'.format(score))   
         plt.scatter(model_xData,model_yData, color='blue')
         plt.plot(model_xData, model.predict(model_xData), color='green', linewidth=1)
-        plt.axis([min(model_data['Returns_x']),max(model_data['Returns_x']),min(model_data['Returns_y']),max(model_data['Returns_y'])])
+        plt.xticks([])
+        plt.yticks([])
+        #plt.axis([min(model_data['Returns_x']),max(model_data['Returns_x']),min(model_data['Returns_y']),max(model_data['Returns_y'])])
         ctr = ctr+1
     plt.show()
 
-start_time = dt.datetime(2017,11,1)
+start_time = dt.datetime(2017,5,1)
 end_time = dt.datetime(2017,11,22)
 #start_time = '2017-11-01'
 #end_time = '2017-11-22'
 tickers = sa_scrape.get_all_ticker_symbols_from_file()
-top_tickers = get_top_coefficients(tickers[:500],25, start_time, end_time)
+top_tickers = get_top_coefficients(tickers[:],25, start_time, end_time,True)
 plot_figures(dict(top_tickers).keys())
